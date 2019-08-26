59e91c0d2a2d109f04ffacb20c85c7edcb8d07fc
BuildTools <unconfigured@null.spigotmc.org>
2019-08-27 00:33:38 +0300

    Start project

--- app/src/main/java/com/rokkystudio/pixella/ToolbarButton.java
+++ app/src/main/java/com/rokkystudio/pixella/ToolbarButton.java
@@ -1,19 +1,16 @@
 package com.rokkystudio.pixella;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
+import android.graphics.DrawFilter;
 import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.PaintFlagsDrawFilter;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
 public class ToolbarButton extends AppCompatImageView
 {
-    private Paint paint = new Paint(Paint.FILTER_BITMAP_FLAG);
-    private Bitmap resized;
+    private static final DrawFilter mDrawFilter = new PaintFlagsDrawFilter(Paint.FILTER_BITMAP_FLAG, 0);
 
     public ToolbarButton(Context context) {
         super(context);
@@ -28,35 +25,8 @@ public class ToolbarButton extends AppCompatImageView
     }
 
     @Override
-    protected void onAttachedToWindow() {
-        super.onAttachedToWindow();
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        int size = Math.min(w, h) - (int) getResources().getDimension(R.dimen.toolbar_padding) * 2;
-        ((BitmapDrawable) getDrawable()).setAntiAlias(false);
-        Bitmap bitmap = ((BitmapDrawable) getDrawable()).getBitmap();
-        getDrawable().
-        resized = Bitmap.createScaledBitmap(bitmap, size, size, false);
-        //resized = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
-        //Matrix matrix = new Matrix();
-        //matrix.postScale(5, 5);
-        //resized = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
-
-        paint.setAntiAlias(false);
-        paint.setDither(true);
-        paint.setFilterBitmap(false);
-    }
-
-    @Override
     protected void onDraw(Canvas canvas) {
-        //canvas.scale(5, 5);
-        canvas.drawBitmap(resized, 0,0, paint);
+        //canvas.setDrawFilter(mDrawFilter);
+        super.onDraw(canvas);
     }
 }
